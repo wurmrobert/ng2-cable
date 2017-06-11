aa25b513ab56a9750f3172ef77df21a0259b0b34
Jose Seravalli Ramos <jseravalli@gmail.com>
2017-06-11 17:39:24 -0600

    Adding support for multiple channels over the same cable.

--- ts/ng2-cable.ts
+++ ts/ng2-cable.ts
@@ -5,25 +5,43 @@ import { Broadcaster } from './broadcaster';
 @Injectable()
 export class Ng2Cable {
   public cable: any;
-  public subscription: any;
+  public channels : any = {};
 
-  constructor(private broadcaster: Broadcaster) {
+  constructor(url) {
+    this.connect(url);
   }
 
-  subscribe(url, channel) {
-    this.setCable(url);
-    this.subscription = this.cable.subscriptions.create(channel, {
-      received: (data) => {
-        this.broadcaster.broadcast((data.action || channel), data);
-      }
-    });
+  subscribe(channel) : Broadcaster {
+    let channelName = (typeof(channel) === 'object') ? channel['channel'] : channel,
+        broadcaster = new Broadcaster(),
+        subscription = this.cable.subscriptions.create(channel, {
+          received: (data) => {
+            broadcaster.broadcast((data.action || channel), data);
+          }
+        });
+    this.channels[channelName] = {
+      subscription: subscription,
+      broadcaster: broadcaster
+    };
+    return broadcaster;
   }
 
-  setCable(url): void {
+  unsubscribe(channel) : void {
+    let subscription = this.channels[channel].subscription;
+    this.cable.subscriptions.remove(subscription);
+  }
+
+  perform(channel, action, data) : void {
+    this.channels[channel].subscription.perform(action, data);
+  }
+
+  connect(url): any {
     this.cable = ActionCable.createConsumer(url);
+    this.cable.connect();
+    return this.cable;
   }
 
-  unsubscribe() {
-    this.cable.subscriptions.remove(this.subscription);
+  disconnect(): void {
+    this.cable.disconnect();
   }
 }
