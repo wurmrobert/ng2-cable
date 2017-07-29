43a1030ee1337e631650865dc678ce6ef75f19f9
Jose Seravalli Ramos <jseravalli@gmail.com>
2017-07-28 21:07:59 -0600

    Changing target to es5

--- js/broadcaster.js
+++ js/broadcaster.js
@@ -1,18 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Rx_1 = require("rxjs/Rx");
-class Broadcaster {
-    constructor() {
+var Rx_1 = require("rxjs/Rx");
+var Broadcaster = (function () {
+    function Broadcaster() {
         this._eventBus = new Rx_1.Subject();
     }
-    broadcast(key, data) {
-        this._eventBus.next({ key, data });
-    }
-    on(key) {
+    Broadcaster.prototype.broadcast = function (key, data) {
+        this._eventBus.next({ key: key, data: data });
+    };
+    Broadcaster.prototype.on = function (key) {
         return this._eventBus.asObservable()
-            .filter(event => event.key === key)
-            .map(event => event.data);
-    }
-}
+            .filter(function (event) { return event.key === key; })
+            .map(function (event) { return event.data; });
+    };
+    return Broadcaster;
+}());
 exports.Broadcaster = Broadcaster;
 //# sourceMappingURL=/vagrant-ng2-cable/broadcaster.js.map
--- js/ng2-cable.js
+++ js/ng2-cable.js
@@ -9,19 +9,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@angular/core");
-const ActionCable = require("actioncable");
-const broadcaster_1 = require("./broadcaster");
-let Ng2Cable = class Ng2Cable {
-    constructor(url) {
+var core_1 = require("@angular/core");
+var ActionCable = require("actioncable");
+var broadcaster_1 = require("./broadcaster");
+var Ng2Cable = (function () {
+    function Ng2Cable(url) {
         this.channels = {};
         this.connect(url);
     }
-    subscribe(channel) {
-        let channelName = (typeof (channel) === 'object') ? channel['channel'] : channel;
-        let broadcaster = new broadcaster_1.Broadcaster();
-        let subscription = this.cable.subscriptions.create(channel, {
-            received: (data) => {
+    Ng2Cable.prototype.subscribe = function (channel) {
+        var channelName = (typeof (channel) === 'object') ? channel['channel'] : channel;
+        var broadcaster = new broadcaster_1.Broadcaster();
+        var subscription = this.cable.subscriptions.create(channel, {
+            received: function (data) {
                 broadcaster.broadcast((data.action || channel), data);
             }
         });
@@ -30,23 +30,24 @@ let Ng2Cable = class Ng2Cable {
             broadcaster: broadcaster
         };
         return broadcaster;
-    }
-    unsubscribe(channel) {
-        let subscription = this.channels[channel].subscription;
+    };
+    Ng2Cable.prototype.unsubscribe = function (channel) {
+        var subscription = this.channels[channel].subscription;
         this.cable.subscriptions.remove(subscription);
-    }
-    perform(channel, action, data) {
+    };
+    Ng2Cable.prototype.perform = function (channel, action, data) {
         this.channels[channel].subscription.perform(action, data);
-    }
-    connect(url) {
+    };
+    Ng2Cable.prototype.connect = function (url) {
         this.cable = ActionCable.createConsumer(url);
         this.cable.connect();
         return this.cable;
-    }
-    disconnect() {
+    };
+    Ng2Cable.prototype.disconnect = function () {
         this.cable.disconnect();
-    }
-};
+    };
+    return Ng2Cable;
+}());
 Ng2Cable = __decorate([
     core_1.Injectable(),
     __metadata("design:paramtypes", [String])
